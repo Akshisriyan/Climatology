ff8aba1aa40338473b90aa60352b3f9b16e722d6
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-01-23 11:17:22 +0530

    commit 1

---
+++ Home.js
@@ -0,0 +1,14 @@
+(function() {
+    "use strict";
+  
+    /** Easy selector helper function*/
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+}
+)
