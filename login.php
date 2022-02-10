88c2574babd79a06e3e23c32b2b7b19e75b22ac7
AbimonFdo <96094976+AbimonFdo@users.noreply.github.com>
2022-02-10 18:53:08 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h2>Login</h2>
+		</div>
+		<form action="login.php" method="post">
+
+
+<div>
+	<label for="UserName">UserName : </label>
+	<input type="text" name="UserName"required >
+</div>
+
+<div>
+	<label for="Password">Password : </label>
+	<input type="password" name="Password1"required>
+</div>
+
+
+
+<button type="submit" name="login">Log in</button>
+<p> Not a User?<a href="registration.php"><b>Register here</b></a></p>
+
+		</form>
+		</div>	
+</body>
+</html>
