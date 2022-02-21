c0badd1aaf54a0fce3d4e3e6ab8db70071a3b93e
Ravindi Jayalath <95970074+RCJayalath@users.noreply.github.com>
2022-02-21 15:57:26 +0530

    update

--- aboutus.js
+++ aboutus.js
@@ -56,4 +56,40 @@
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
   }
+  
+  
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  
+
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
 
