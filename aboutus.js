f37b755805e213aad62e91684e0f1f67834189e0
Ravindi Jayalath <95970074+RCJayalath@users.noreply.github.com>
2022-02-21 15:55:21 +0530

    function1add

---
+++ aboutus.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
