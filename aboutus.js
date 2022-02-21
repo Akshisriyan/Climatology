9269466acb9f41f693272174dc2cdeebbb1da129
Ravindi Jayalath <95970074+RCJayalath@users.noreply.github.com>
2022-02-21 15:56:47 +0530

    update

--- aboutus.js
+++ aboutus.js
@@ -41,3 +41,19 @@
       behavior: 'smooth'
     })
   }
+  
+  
+  
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
